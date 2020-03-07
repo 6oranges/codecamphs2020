c8fd5db18c69ff9dc7834bfe9a662eed307cbdfb
Antoine <31717032+AntoineOMEGA@users.noreply.github.com>
2020-03-07 16:45:00 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ Vue.config.ignoredElements = [
 
 Vue.component('vue-player', {
     props: ['index', 'player'],
-    template: `<a-entity geometry="primitive: sphere; segmentsWidth: 4; segmentsHeight: 4; radius: 1;" color="ff0000" position="player.x + ' ' + player.y + ' ' + player.z"></a-entity>`
+    template: `<a-entity geometry="primitive: sphere; segmentsWidth: 4; segmentsHeight: 4; radius: 1;" color="ff0000" :position="player.x + ' ' + player.y + ' ' + player.z"></a-entity>`
 });
 
 var app = new Vue({
